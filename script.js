e2859101990b7b1552ed5d2fc7df3775a470e581
DanielZornek <daniel_zornek91@outlook.com>
2024-02-11 23:48:51 -0300

    finished the principle of the game with a final score info

--- script.js
+++ script.js
@@ -12,6 +12,33 @@ function getComputerChoice(){
 
 // console.log(`The choice is ${getComputerChoice()}`);
 // Return ok
+let actualWinner;
+
+let defineWinner = (userChoice, computerChoice) => {
+
+    if(userChoice === "rock" && computerChoice === "scissors"){
+        actualWinner = "user";
+        return `${userChoice} smashes ${computerChoice}, YOU WIN!!!`;
+    }else if(userChoice === "paper" && computerChoice === "rock"){
+        actualWinner = "user";
+        return `${userChoice} covers ${computerChoice}, YOU WIN!!!`;
+    }else if(userChoice === "scissors" && computerChoice === "paper"){
+        actualWinner = "user";
+        return `${userChoice} cuts ${computerChoice}, YOU WIN!!!`;
+    }else if(computerChoice === "rock" && userChoice === "scissors"){
+        actualWinner = "computer";
+        return `${computerChoice} smashes ${userChoice}, You Lose!!!`;
+    }else if(computerChoice === "paper" && userChoice === "rock"){
+        actualWinner = "computer";
+        return `${computerChoice} covers ${userChoice}, You Lose!!!`;
+    }else if(computerChoice === "scissors" && userChoice === "paper"){
+        actualWinner = "computer";
+        return `${computerChoice} cuts ${userChoice}, You Lose!!!`;
+    }else{
+        actualWinner = "draw";
+        return `Both choosed ${userChoice}. We have a Draw!`;
+    }
+}
 
 function playRound(userChoice, computerChoice){
     // Verifying the user input
@@ -25,28 +52,44 @@ function playRound(userChoice, computerChoice){
 
     console.log(`Your Choose: ${userChoice}\nOponent Choose: ${computerChoice}`);
 
-    let defineWinner = () => {
+    let newWinner = defineWinner(userChoice, computerChoice);
+    return newWinner;
+}
 
-        if(userChoice === "rock" && computerChoice === "scissors"){
-            return `${userChoice} smashes ${computerChoice}, YOU WIN!!!`;
-        }else if(userChoice === "paper" && computerChoice === "rock"){
-            return `${userChoice} covers ${computerChoice}, YOU WIN!!!`;
-        }else if(userChoice === "scissors" && computerChoice === "paper"){
-            return `${userChoice} cuts ${computerChoice}, YOU WIN!!!`;
-        }else if(computerChoice === "rock" && userChoice === "scissors"){
-            return `${computerChoice} smashes ${userChoice}, You Lose!!!`;
-        }else if(computerChoice === "paper" && userChoice === "rock"){
-            return `${computerChoice} covers ${userChoice}, You Lose!!!`;
-        }else if(computerChoice === "scissors" && userChoice === "paper"){
-            return `${computerChoice} cuts ${userChoice}, You Lose!!!`;
-        }else{
-            return `Both choosed ${userChoice}. We have a Draw!`;
+let computerWins = 0; 
+let userWins = 0;
+let drawCounts = 0;
+let winnerGame;
+
+function playGame(){
+    for(let i = 0; i < 5; i++){
+        console.log(playRound(prompt("choose: | Rock | Paper| Scissors |").toLowerCase(), getComputerChoice()));
+
+        if(actualWinner === "user"){
+            userWins++; 
+        }else if(actualWinner === "computer"){
+            computerWins++;
+        }else if(actualWinner === "draw"){
+            drawCounts++;
         }
     }
 
-    return defineWinner();
+    if(userWins > computerWins){
+        winnerGame = "user";
+    }else if(computerWins > userWins){
+        winnerGame = "computer";
+    }else{
+        winnerGame = "Draw";
+    }
+
+    return `
+                        Final of Game\n
+                      Here Are the results
+        Winner: ${winnerGame}
+        User Total Wins: ${userWins}
+        Computer Total Wins: ${computerWins}
+        Draws: ${drawCounts}           
+    `;
 }
 
-console.log(playRound(prompt("choose: | Rock | Paper| Scissors |").toLowerCase(), getComputerChoice()));
-
-console.log("Maybe everything is cool...");
+console.log(playGame());
